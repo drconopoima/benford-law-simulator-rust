33bf7d247ee5ac328d06dacbe8710f196808f15c
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2020-11-22 01:52:38 +0100

    Add textplot chart for number of digits

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use structopt::StructOpt;
 use rand::Rng;
 use std::collections::HashMap;
+use textplots::{Chart, Plot, Shape};
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -16,7 +17,7 @@ struct Opt {
     debug: bool,
 }
 
-fn generate_sample(max_value: usize, sample_size: usize, min_value: usize) -> Vec<usize> {
+fn generate_sample(max_value: usize, sample_size: usize, min_value: usize, digit_count:u8) -> Vec<usize> {
     let mut sample: Vec<usize> = Vec::new();
     let division_denominator: usize = max_value + 1 - min_value;
     let target_chance:f32 = (1.0)/(division_denominator as f32);
@@ -24,15 +25,19 @@ fn generate_sample(max_value: usize, sample_size: usize, min_value: usize) -> Ve
     let mut rng=rand::thread_rng();
     let mut pushed_values:usize=0;
     let mut current_size:usize;
+    let ten:u8=10;
+    let power:u8=ten.pow((digit_count-1).into());
     for _ in 0..sample_size {
         current_size=pushed_values;
         while current_size==pushed_values {
             for current_value in min_value..=max_value {
                 float_0_1 = rng.gen();
                 if float_0_1 < target_chance {
-                    sample.push(current_value);
-                    pushed_values+=1;
-                    break;
+                    if current_value>power.into() {
+                        sample.push(current_value);
+                        pushed_values+=1;
+                        break;
+                    }
                 }
             };
         };
@@ -58,19 +63,27 @@ fn main() {
     let max=args.max;
     let sample_size=args.sample_size;
     let min=args.min;
-    let lead_digits=args.lead_digits;
-    let sample: Vec<usize> = generate_sample(max, sample_size, min);
+    let num_lead_digits=args.lead_digits;
+    let sample: Vec<usize> = generate_sample(max, sample_size, min, num_lead_digits);
     if debug { println!("Simulated sample data:\n{:?}", sample);}
-    let lead_digits: Vec<u16> = vector_lead_n_digits(sample, lead_digits, sample_size);
-    if debug { println!("Lead digits:\n{:?}", lead_digits);}
+    let lead_digits_vec: Vec<u16> = vector_lead_n_digits(sample, num_lead_digits, sample_size);
+    if debug { println!("Lead digits:\n{:?}", lead_digits_vec);}
     let mut repeated_counts:HashMap<usize,usize> = HashMap::new();
     for index in 0..sample_size {
-        let usize_index:usize=usize::from(lead_digits[index]);
-        if repeated_counts.contains_key(&usize_index) {
-            *repeated_counts.get_mut(&usize_index).unwrap()+=1;
+        let lead_digits_index:usize=usize::from(lead_digits_vec[index]);
+        if repeated_counts.contains_key(&lead_digits_index) {
+            *repeated_counts.get_mut(&lead_digits_index).unwrap()+=1;
         } else {
-            repeated_counts.insert(usize_index, 1);
+            repeated_counts.insert(lead_digits_index, 1);
         }
     }
-    println!("{:?}", repeated_counts)
+    let mut points:Vec<(f32, f32)>=Vec::new();
+    let ten:u16=10;
+    let power:u16=ten.pow((num_lead_digits-1).into());
+    let top_count=(ten.pow(num_lead_digits.into()))-1;
+    for i in power..=top_count.into() {
+        if repeated_counts.contains_key(&usize::from(i)) { points.push((i as f32,repeated_counts[&usize::from(i)] as f32))}
+    }
+    println!("Lead digits counts:\n{:?}", points);
+    Chart::new(180, 60, power.into(), top_count as f32).lineplot(&Shape::Steps(&points)).nice();
 }
