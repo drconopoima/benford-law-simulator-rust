2d03309aafb4d8e30bd583f219867b02507ae8bc
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2020-11-22 21:27:24 +0100

    Refactor repeated code plot digits lower/upper bound

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ struct Opt {
     debug: bool,
 }
 
-fn generate_sample(max_value: usize, sample_size: usize, min_value: usize, digit_count:u8) -> Vec<usize> {
+fn generate_sample(max_value: usize, sample_size: usize, min_value: usize, lower_boundary:u16) -> Vec<usize> {
     let mut sample: Vec<usize> = Vec::new();
     let division_denominator: usize = max_value + 1 - min_value;
     let target_chance:f32 = (1.0)/(division_denominator as f32);
@@ -25,15 +25,13 @@ fn generate_sample(max_value: usize, sample_size: usize, min_value: usize, digit
     let mut rng=rand::thread_rng();
     let mut pushed_values:usize=0;
     let mut current_size:usize;
-    let ten:u8=10;
-    let power:u8=ten.pow((digit_count-1).into());
     for _ in 0..sample_size {
         current_size=pushed_values;
         while current_size==pushed_values {
             for current_value in min_value..=max_value {
                 float_0_1 = rng.gen();
                 if float_0_1 < target_chance {
-                    if current_value>power.into() {
+                    if current_value>lower_boundary.into() {
                         sample.push(current_value);
                         pushed_values+=1;
                         break;
@@ -57,6 +55,16 @@ fn vector_lead_n_digits(sample:Vec<usize>, digit_count:u8, sample_size: usize) -
     return vector_lead_digits
 }
 
+fn lower_digits_boundary(base:u16, count_lead_digits:u32) -> u16 {
+    let lower_boundary:u16=base.pow(count_lead_digits-1);
+    return lower_boundary
+}
+
+fn upper_digits_boundary(base: u16, count_lead_digits:u32) -> u16 {
+    let upper_boundary=(base.pow(count_lead_digits))-1;
+    return upper_boundary
+}
+
 fn main() {
     let args=Opt::from_args();
     let debug=args.debug;
@@ -64,7 +72,10 @@ fn main() {
     let sample_size=args.sample_size;
     let min=args.min;
     let num_lead_digits=args.lead_digits;
-    let sample: Vec<usize> = generate_sample(max, sample_size, min, num_lead_digits);
+    let base_ten:u16=10;
+    let lower_boundary:u16 = lower_digits_boundary(base_ten, num_lead_digits.into());
+    let upper_boundary:u16 = upper_digits_boundary(base_ten, num_lead_digits.into());
+    let sample: Vec<usize> = generate_sample(max, sample_size, min, lower_boundary);
     if debug { println!("Simulated sample data:\n{:?}", sample);}
     let lead_digits_vec: Vec<u16> = vector_lead_n_digits(sample, num_lead_digits, sample_size);
     if debug { println!("Lead digits:\n{:?}", lead_digits_vec);}
@@ -78,12 +89,9 @@ fn main() {
         }
     }
     let mut points:Vec<(f32, f32)>=Vec::new();
-    let ten:u16=10;
-    let power:u16=ten.pow((num_lead_digits-1).into());
-    let top_count=(ten.pow(num_lead_digits.into()))-1;
-    for i in power..=top_count.into() {
+    for i in lower_boundary..=upper_boundary.into() {
         if repeated_counts.contains_key(&usize::from(i)) { points.push((i as f32,repeated_counts[&usize::from(i)] as f32))}
     }
     println!("Lead digits counts:\n{:?}", points);
-    Chart::new(180, 60, power.into(), top_count as f32).lineplot(&Shape::Steps(&points)).nice();
+    Chart::new(180, 60, lower_boundary.into(), upper_boundary as f32).lineplot(&Shape::Steps(&points)).nice();
 }
